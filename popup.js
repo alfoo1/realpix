d213767e079f14c6aa25db3b84c89aff24df496a
Victor Prado <vmpradob@gmail.com>
2025-10-04 17:09:29 -0300

    feat: connect front with back

--- popup.js
+++ popup.js
@@ -1,36 +1,21 @@
-import { check_file } from './src/api.js';
-document.addEventListener('DOMContentLoaded', async function() {
-<<<<<<< HEAD
-    // Obtiene el parámetro 'url' de la URL
-    const params = new URLSearchParams(window.location.search);
-    const url = params.get('url');
-
-    if (url) {
-        const authors = await check_file(url);
-        console.log('Authors:', authors);
-        
-        for (let i = 0; i < authors.length; i++) {
-
-            document.getElementById('URL').innerHTML = '<a href="">'+url+'</a>';
-            document.getElementById('autor').innerHTML = authors[i].name;
-            let action = '';
-            for (let j = 0; j < authors[i].actions.length; j++) {
-                action += authors[i].actions[j] + ', ';
-            }
-            document.getElementById('acciones').innerHTML = ''; 
-        }
-
-    }
-});
-=======
+import { check_file } from "./src/api.js";
+document.addEventListener("DOMContentLoaded", async function () {
   // Obtiene el parámetro 'url' de la URL
   const params = new URLSearchParams(window.location.search);
-  const url = params.get('url');
+  const url = params.get("url");
 
   if (url) {
-    document.getElementById('URL').innerHTML = '<a href="">'+url+'</a>';
-    const response = await check_file(url)
-    alert(response[0].name)
+    const authors = await check_file(url);
+    console.log("Authors:", authors);
+
+    for (let i = 0; i < authors.length; i++) {
+      document.getElementById("autor").innerHTML = authors[i].name;
+      let action = "";
+      for (let j = 0; j < authors[i].actions.length; j++) {
+        action += authors[i].actions[j] + "<br>";
+      }
+
+      document.getElementById("acciones").innerHTML = action;
+    }
   }
 });
->>>>>>> 6bbb9e95be96db0bc09ed625ca05b3406f86ee74
