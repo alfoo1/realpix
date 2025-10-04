d199b774bd748b80af87b8ca0a1c9d4224284008
diego <diego@debian-BULLSEYE-live-builder-AMD64>
2025-10-04 17:04:50 -0300

    ultimo front

--- popup.js
+++ popup.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Obtiene el parámetro 'url' de la URL
-  const params = new URLSearchParams(window.location.search);
-  const url = params.get('url');
+import { check_file } from './src/api.js';
+document.addEventListener('DOMContentLoaded', async function() {
+    // Obtiene el parámetro 'url' de la URL
+    const params = new URLSearchParams(window.location.search);
+    const url = params.get('url');
 
-  if (url) {
-    document.getElementById('URL').innerHTML = '<a href="">'+url+'</a>';
-  }
+    if (url) {
+        const authors = await check_file(url);
+        console.log('Authors:', authors);
+        
+        for (let i = 0; i < authors.length; i++) {
 
-  const helloButton = document.getElementById('helloButton');
-  helloButton.addEventListener('click', function() {
-    chrome.storage.local.get('lastContextUrl', function(data) {
-      if (data.lastContextUrl) {
-        document.getElementById('URL').innerHTML = '<a href="">'+data.lastContextUrl+'</a>';
-    //     alert('URL seleccionada: ' + data.lastContextUrl);
-      } else {
-        alert('No hay URL seleccionada aún.');
-      }
-    });
-  });
+            document.getElementById('URL').innerHTML = '<a href="">'+url+'</a>';
+            document.getElementById('autor').innerHTML = authors[i].name;
+            let action = '';
+            for (let j = 0; j < authors[i].actions.length; j++) {
+                action += authors[i].actions[j] + ', ';
+            }
+            document.getElementById('acciones').innerHTML = ''; 
+        }
+
+    }
 });
